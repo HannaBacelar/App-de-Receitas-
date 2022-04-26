9cc85855f98c6080d9f51583f94bdac88bc3bf7c
Luá Octaviano <luacomacento@gmail.com>
2022-04-26 16:28:25 -0300

    feat: validação do login e redirecionamento

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,65 @@
 import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 function Login() {
+  const [login, setLogin] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [isButtonDisabled, setButtonDisabled] = useState(true);
+  const [password, setPassword] = useState('');
+
+  const verifyForm = () => {
+    const emailFormat = /[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z.]*\w$/;
+    const minSize = 6;
+    const isEmailValid = emailFormat.test(email);
+    const isPasswordValid = password.length >= minSize;
+    setButtonDisabled(!(isEmailValid && isPasswordValid));
+  };
+
+  const handleInputChange = (target) => {
+    if (target.name === 'email') setEmail(target.value);
+    if (target.name === 'password') setPassword(target.value);
+    verifyForm();
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify({ email }));
+    setLogin(true);
+  };
+
   return (
-    <>
+    <form>
       <input
         type="text"
         data-testid="email-input"
+        name="email"
+        onChange={ ({ target }) => handleInputChange(target) }
         placeholder="E-mail"
       />
 
       <input
         type="password"
+        onChange={ ({ target }) => handleInputChange(target) }
         data-testid="password-input"
+        name="password"
         placeholder="Senha"
       />
 
       <button
+        disabled={ isButtonDisabled }
+        onClick={ handleClick }
         type="submit"
         data-testid="login-submit-btn"
       >
         Enter
       </button>
-    </>
+      {
+        login && <Redirect to="/foods" />
+      }
+    </form>
   );
 }
 
