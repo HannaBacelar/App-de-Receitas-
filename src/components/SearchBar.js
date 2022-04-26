281a8bd797a85fb09bde12239cde144d290e7910
Luá Octaviano <luacomacento@gmail.com>
2022-04-26 18:51:02 -0300

    feat: estruturação dos radio buttons de busca
    Co-authored-by: Matheus Martins Pigatto <MatheusmPigatto@users.noreply.github.com>
    Co-authored-by: Joarez Ximenes <JoarezXimenes@users.noreply.github.com>
    Co-authored-by: Hanna Bacelar <HannaBacelar@users.noreply.github.com>
    Co-authored-by: Thais Oliveira  <victoriaatlas@users.noreply.github.com>

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,47 @@ import React from 'react';
 
 function SearchBar() {
   return (
-    <input type="text" data-testid="search-input" />
+    <div>
+      <input type="text" data-testid="search-input" />
+      <div>
+        <label htmlFor="ingredient-search-radio">
+          <input
+            type="radio"
+            name="searchRadio"
+            id="ingredient-search-radio"
+            data-testid="ingredient-search-radio"
+          />
+          Ingredient
+        </label>
+
+        <label htmlFor="name-search-radio">
+          <input
+            type="radio"
+            name="searchRadio"
+            id="name-search-radio"
+            data-testid="name-search-radio"
+          />
+          Name
+        </label>
+
+        <label htmlFor="first-letter-search-radio">
+          <input
+            type="radio"
+            name="searchRadio"
+            id="first-letter-search-radio"
+            data-testid="first-letter-search-radio"
+          />
+          First Letter
+        </label>
+
+        <button
+          type="button"
+          data-testid="exec-search-btn"
+        >
+          Search
+        </button>
+      </div>
+    </div>
   );
 }
 
