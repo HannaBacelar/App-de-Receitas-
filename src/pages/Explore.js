8599ee5bf72f70eef351c652f6cc9003a62e2c60
JoarezXimenes <joarezximenesleitejunior@gmail.com>
2022-05-09 19:37:13 -0300

    style: css da pagina explore

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GiKnifeFork } from 'react-icons/gi';
+import { BiDrink } from 'react-icons/bi';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import '../styles/Explore.css';
 
 function Explore() {
   return (
-    <div>
+    <div className="Explore">
       <Header
         pageTitle="Explore"
       />
-      <Link to="/explore/foods">
-        <button data-testid="explore-foods" type="button">Explore Foods</button>
-      </Link>
-      <Link to="/explore/drinks">
-        <button type="button" data-testid="explore-drinks">Explore Drinks</button>
-      </Link>
+      <main>
+        <Link className="explore-link" to="/explore/foods">
+          <button data-testid="explore-foods" type="button">
+            <GiKnifeFork className="explore-link-icon" />
+            Explore Foods
+          </button>
+        </Link>
+        <Link className="explore-link" to="/explore/drinks">
+          <button type="button" data-testid="explore-drinks">
+            <BiDrink className="explore-link-icon" />
+            Explore Drinks
+          </button>
+        </Link>
+      </main>
       <Footer />
     </div>
   );
