bbb9e161303e99bcfd6362363bf1b2cabe893699
JoarezXimenes <joarezximenesleitejunior@gmail.com>
2022-05-10 17:33:54 -0300

    style: css das paginas de explore

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ExploreButtons from '../components/ExploreButtons';
+import '../styles/Explore.css';
 
 function ExploreDrinks() {
   return (
-    <div>
+    <div className="Explore">
       <Header
         pageTitle="Explore Drinks"
       />
-      <ExploreButtons page="Drinks" link="drinks" />
+      <main>
+        <ExploreButtons page="Drinks" link="drinks" />
+      </main>
       <Footer />
     </div>
   );
